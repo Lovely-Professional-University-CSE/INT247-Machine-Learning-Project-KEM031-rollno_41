b010dda230457111dc4d62247e54eff8ea3d72b7
vishu <vishu.damahe1999@gmail.com>
2020-04-06 17:00:51 +0530

    Created dataframe and selected some features out of 91 features

--- code_for_project.py
+++ code_for_project.py
@@ -3,3 +3,13 @@ import numpy as np
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 %matplotlib inline
+
+#reading the dataset crime_data.csv
+crime_df=pd.read_csv('crime_data.csv')
+print(crime_df.head())
+print(crime_df.columns)
+
+#Selecting some of the important features
+crime_data=crime_df[['States/UTs','District','Year','Murder','Rape','Dacoity','Riots','Forgery','Acid attack','Sexual Harassment','Incidence of Rash Driving','HumanTrafficking','Total Cognizable IPC crimes']]
+print(crime_data.head())
+
